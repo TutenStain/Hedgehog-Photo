16c1e1de7b3241b124a7476651acbf870d0c8e72
Florian Minges <florianminges@hotmail.com>
2012-03-16 22:48:20 +0100

    Skapade ett eclipse-projekt

---
+++ src/se/cth/hedgehogphoto/Main.java
@@ -0,0 +1,14 @@
+package se.cth.hedgehogphoto;
+
+/**
+ * Main class.
+ * @author Grupp 1
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
