410d3337037353f039c986479a670a037c9b2dd0
Florian Minges <florianminges@hotmail.com>
2012-03-18 17:47:22 +0100

    Better implementation of the metadata-reader
    Can now filter the metadata for specific properties and print this.
    Some work still needs to be done though.

--- Hedgehog Photo/src/se/cth/hedgehogphoto/metadata/Metadata.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/metadata/Metadata.java
@@ -1,9 +1,13 @@
 package se.cth.hedgehogphoto.metadata;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.sanselan.ImageInfo;
 import org.apache.sanselan.ImageReadException;
 import org.apache.sanselan.Sanselan;
 import org.apache.sanselan.common.IImageMetadata;
@@ -14,16 +18,25 @@ import org.apache.sanselan.common.IImageMetadata;
  */
 public class Metadata {
 	
+	private static final String[] metadataTypes = 
+			{"Modify Date", "Artist", "XPComment", "XPAuthor", 
+					"XPKeywords", "Date Time Original"};
+	
 	public static void main(String [] args) {
-		extract();
+		try {
+			searchTag(extract());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	public static void extract(File file) {
-		
-	}
-	
-	public static void extract() {
+	public static String extract() {
 		File file = new File("finbild.jpg");
+		return extract(file);
+	}
+	
+	public static String extract(File file) {
 		IImageMetadata metadata = null;
 		try {
 			metadata = Sanselan.getMetadata(file);
@@ -32,11 +45,41 @@ public class Metadata {
 			e.printStackTrace();
 		}
 		
-		ArrayList items = metadata.getItems();
-		System.out.println(metadata.toString()+"\n\n");
+		return metadata.toString();
+	}
+	
+	public static void searchTag(String text) throws IOException {
+		BufferedReader br = new BufferedReader( new StringReader(text) );
+		List<String> foundTypes = new ArrayList<String>();
 		
-		for(int i = 0; i<items.size();i++) {
-			System.out.println(items.get(i).toString());
+		String line;
+		int nbrOfTypes = metadataTypes.length;
+		while((line = br.readLine()) != null) {		
+			if (containsTargetMetadata(line)) {
+				foundTypes.add(line);
+			}
+		}
+		
+		printList(foundTypes);
+	}
+	
+	private static boolean containsTargetMetadata(String line) {
+		int nbrOfTypes = metadataTypes.length;
+		for(int i = 0; i < nbrOfTypes; i++) {
+			if(line.contains(metadataTypes[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	private static void printList(List<String> list) {
+		for(int i = 0; i<list.size(); i++) {
+			System.out.println(list.get(i));
 		}
 	}
+	
+	
 }
