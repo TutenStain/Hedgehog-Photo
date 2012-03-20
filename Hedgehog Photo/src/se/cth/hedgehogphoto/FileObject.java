41ba0c6f85093b8072970f95b41583146133e23d
Barnabas Sapan <barnabas.sapan@gmail.com>
2012-03-20 13:30:50 +0100

    Updated interfaces according to our "UML".

---
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/FileObject.java
@@ -0,0 +1,10 @@
+package se.cth.hedgehogphoto;
+
+public interface FileObject {
+	public String getTags();
+	public String getDate();
+	public String getComment();
+	public String getLocation();
+	public String getImageName();
+	public String getImagePath();
+}
--- Hedgehog Photo/src/se/cth/hedgehogphoto/metadata/ImageDataImpl.java
+++
@@ -1,8 +0,0 @@
-package se.cth.hedgehogphoto.metadata;
-
-public interface ImageDataImpl {
-	public String getGeoLocation();
-	public String getDate();
-	public String getImageName();
-	public String getImagePath();
-}
