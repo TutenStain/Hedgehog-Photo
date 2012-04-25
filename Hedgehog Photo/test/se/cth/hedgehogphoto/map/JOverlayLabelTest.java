a7738b552b27ffc9af50826cbe97e27f676227f3
FlorianMinges <florianminges@hotmail.com>
2012-04-25 16:00:43 +0200

    Updated build-path and created some test-method-stubs

---
+++ Hedgehog Photo/test/se/cth/hedgehogphoto/map/JOverlayLabelTest.java
@@ -0,0 +1,119 @@
+package se.cth.hedgehogphoto.map;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JOverlayLabelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetXPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetYPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetXOffset() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetYOffset() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPixelPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLabelPointsFromLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLabelPointsFromTop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPropertyChange() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMouseListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetImageIcon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetImageIcon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIconHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIconHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIconWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIconWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProperIconSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIntersects() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBoundsIntInt() {
+		fail("Not yet implemented");
+	}
+
+}
