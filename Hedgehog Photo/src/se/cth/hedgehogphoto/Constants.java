c11087eec238efd3c9014504c1a2a2a4680fd8be
Barnabas Sapan <barnabas.sapan@gmail.com>
2012-05-13 09:06:39 +0200

    Added a (temporary) boot-image and caption to the window.

--- Hedgehog Photo/src/se/cth/hedgehogphoto/Constants.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/Constants.java
@@ -3,6 +3,8 @@ package se.cth.hedgehogphoto;
 public interface Constants {
 	public static final int PREFERRED_WINDOW_HEIGHT = 1000;
 	public static int PREFERRED_WINDOW_WIDTH = 500;
+	public static final int PREFERRED_STARTUP_WINDOW_WIDTH = 463;
+	public static final int PREFERRED_STARTUP_WINDOW_HEIGHT = 380;
 	public static final int PREFERRED_MODULE_WIDTH = 300; //Width of the left panel-modules
 	public static final int PREFERRED_MODULE_HEIGHT = 300; //Height of the left panel-modules
 	/* TODO: Add global accessible variables here. */
--- Hedgehog Photo/src/se/cth/hedgehogphoto/view/StartUpView.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/view/StartUpView.java
@@ -7,22 +7,34 @@ import java.util.logging.Level;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import se.cth.hedgehogphoto.Constants;
 import se.cth.hedgehogphoto.database.DatabaseHandler;
 import se.cth.hedgehogphoto.log.Log;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
 
 /**
- * 
+ * A simple startup/loaing screen that gets
+ * displayed until the database is up and running.
+ * This is also the part of the program where the main
+ * frame is constructed and initialized. 
  * @author Barnabas Sapan
- *
  */
 
 public class StartUpView extends JFrame implements Runnable{
-
+	
+	public StartUpView(){
+		super("Hedgehog Photo");
+	}
+	
 	@Override
 	public void run(){
-		setSize(new Dimension(300, 300));
+		setSize(new Dimension(Constants.PREFERRED_STARTUP_WINDOW_WIDTH, Constants.PREFERRED_STARTUP_WINDOW_HEIGHT));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		add(new JLabel("LOADING..."));
+		
+		JLabel image = new JLabel();
+		image.setIcon(new ImageIcon(StartUpView.class.getResource("/se/cth/hedgehogphoto/resources/boot.gif")));
+		add(image);
 		
 		//Center the screen
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
