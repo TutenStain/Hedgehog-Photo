4153040c524286c2c2a94eb9eaea380785fba2f2
David Grankvist <david.grankvist@hotmail.com>
2012-05-14 16:20:20 +0200

    Added instanceof checks in Note controller class

--- Hedgehog Photo/src/se/cth/hedgehogphoto/note/NoteController.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/note/NoteController.java
@@ -14,36 +14,44 @@ import javax.swing.event.ChangeListener;
 public class NoteController {
 	private NoteModel model;
 	private NoteView view;
-	
+
 	public NoteController(NoteModel m, NoteView v){
 		model = m;
 		view = v;
-		
+
 		view.setSliderListener(new ChangeListener(){
 			@Override
 			public void stateChanged(ChangeEvent arg0) {
-				JSlider slider = (JSlider)arg0.getSource();
-				model.setCircleDiam(slider.getValue());
+				if(arg0.getSource() instanceof JSlider){
+					JSlider slider = (JSlider)arg0.getSource();
+					model.setCircleDiam(slider.getValue());
+				}
 			}
 		});
 		view.setColorChooserListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				JButton button = (JButton) arg0.getSource();
-				model.setColor(button.getBackground());
+				if(arg0.getSource() instanceof JButton){
+					JButton button = (JButton) arg0.getSource();
+					model.setColor(button.getBackground());
+				}
 			}
-			
+
 		});
 		view.setDrawAreaListeners(new MouseAdapter(){
 			public void mousePressed(MouseEvent me) {
-				JPanel drawArea = (JPanel)me.getSource();
-				PaintUtils.paintCircle(drawArea.getGraphics(), me.getX(), me.getY(), model.getCircleDiam(), model.getColor());
+				if(me.getSource() instanceof JPanel){
+					JPanel drawArea = (JPanel)me.getSource();
+					PaintUtils.paintCircle(drawArea.getGraphics(), me.getX(), me.getY(), model.getCircleDiam(), model.getColor());
+				}
 			}
 		}, new MouseAdapter(){
 			public void mouseDragged(MouseEvent me){
-				JPanel drawArea = (JPanel)me.getSource();
-				PaintUtils.paintCircle(drawArea.getGraphics(), me.getX(), me.getY(), model.getCircleDiam(), model.getColor());
+				if(me.getSource() instanceof JPanel){
+					JPanel drawArea = (JPanel)me.getSource();
+					PaintUtils.paintCircle(drawArea.getGraphics(), me.getX(), me.getY(), model.getCircleDiam(), model.getColor());
+				}
 			}
 		});
 	}
