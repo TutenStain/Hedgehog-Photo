74aabcf8ee9de6bb289d9bcfeb775abac8343c31
FlorianMinges <florianminges@hotmail.com>
2012-05-14 11:26:17 +0200

    Cleanup, refactored one method

--- Hedgehog Photo/src/se/cth/hedgehogphoto/objects/ImageObject.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/objects/ImageObject.java
@@ -2,6 +2,9 @@ package se.cth.hedgehogphoto.objects;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+
+import se.cth.hedgehogphoto.log.Log;
 
 
 
@@ -10,48 +13,31 @@ public class ImageObject implements FileObject {
 	private String filePath, fileName, date, artist, coverPath,comment, albumName,location = "";
 	private List<String> tags;
 	private String tag;
-	public String getTag() {
-		return tag;
-	}
 
-	public void setTag(String tag) {
-		this.tag = tag;
-	}
-
-
-//	private String longitude, latitude;
 	private LocationObject locationObject = new LocationObject("");
 	private boolean legitGPSInfo;
-	
+
 	public ImageObject() {
 		//init
-	}
-	
-	public String getAlbumName() {
-		return albumName;
-	}
-
-	public void setAlbumName(String albumName) {
-		this.albumName = albumName;
 	}
 
 	public void setProperty(String property, String value) {
 		switch(property) {
-			case "Modify Date": setDate(value); break;
-			case "Artist": setArtist(value); break;
-			case "XPComment": setComment(value); break;
-			case "XPKeywords": setTag(value); break;
-			case "File Path": setFilePath(value); break;
-			case "File Name": setFileName(value); break;
-			case "Interop Index": setFirstGPSDirection(value); break;
-			case "Interop Version": setLatitude(value); break;
-			case "Unknown Tag (0x3)": setSecondGPSDirection(value); break;
-			case "Unknown Tag (0x4)": setLongitude(value); break;
-			
-			default: break;
+		case "Modify Date": setDate(value); break;
+		case "Artist": setArtist(value); break;
+		case "XPComment": setComment(value); break;
+		case "XPKeywords": setTag(value); break;
+		case "File Path": setFilePath(value); break;
+		case "File Name": setFileName(value); break;
+		case "Interop Index": setFirstGPSDirection(value); break;
+		case "Interop Version": setLatitude(value); break;
+		case "Unknown Tag (0x3)": setSecondGPSDirection(value); break;
+		case "Unknown Tag (0x4)": setLongitude(value); break;
+
+		default: break;
 		}
 	}
-	
+
 	public void print() {
 		System.out.println("date: " + date);
 		System.out.println("fileName: " + fileName);
@@ -60,6 +46,22 @@ public class ImageObject implements FileObject {
 		System.out.println("comment: " + comment);
 		System.out.println("tags: " + tags);
 		System.out.println("location: " + location.toString());
+	}
+
+	public String getAlbumName() {
+		return albumName;
+	}
+
+	public void setAlbumName(String albumName) {
+		this.albumName = albumName;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
 	}
 
 	public List<String> getTags() {
@@ -101,38 +103,37 @@ public class ImageObject implements FileObject {
 	public  void setFilePath(String filePath) {
 		this.filePath = filePath;
 	}
-	
+
 	public String getFileName() {
 		return fileName;
 	}
-	
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
-	
+
 	public LocationObject getLocationObject() {
 		return locationObject;
 	}
-	public String getLocation(){
+	public String getLocation() {
 		return location;
 	}
 
-	
 	private void setLongitude(String longitude) {
 		if (legitGPSInfo)
 			locationObject.setLongitude(longitude);
 	}
-	
+
 	private void setLatitude(String latitude) {
 		if (legitGPSInfo)
 			locationObject.setLatitude(latitude);
 	}
-	
+
 	private void setFirstGPSDirection(String value) {
 		value = value.trim();
 		legitGPSInfo = value.equals("\'N\'") || value.equals("\'S\'");
 	}
-	
+
 	private void setSecondGPSDirection(String value) {
 		value = value.trim();
 		legitGPSInfo = value.equals("\'W\'") || value.equals("\'E\'");
@@ -142,7 +143,7 @@ public class ImageObject implements FileObject {
 	public void setLocationObject(LocationObject locationObject) {
 		this.locationObject=locationObject;
 		this.location=locationObject.getLocation();
-		
+
 	}
 
 	@Override
@@ -150,64 +151,63 @@ public class ImageObject implements FileObject {
 		return "[name=" + fileName + "] [location=" + location + "] [date=" + date + "] [tag=" + tags + "] [comments= " + comment +"] [albumName=" + albumName + "]" + "Path= " + filePath + "]";
 	}
 
-	
-	public void convertComment(){
-		String string = "";
-		List<Integer> i = new ArrayList<Integer>();
-	//	io.getComment().substring(1);
-		try{
-		  for(String s:comment.substring(1).split(", ")){
-			  i.add(Integer.parseInt(s));
-		  }
-		  for(Integer ii: i){
 
-		  String aChar = new Character((char)(int)ii).toString();
-		//  if(!(aChar.equals("")))
-		  string = string + aChar;
-		  string = string.trim();
-		 
-		  }
-		}catch(Exception e){
+	public void convertComment() {
+		try {
+			this.comment = convertDecimalNumbersToString(this.comment);
 		}
-	this.comment =string;
-	}
-	public void convertTags(){
-		String string = "";
-		List<Integer> i = new ArrayList<Integer>();
-	//	io.getComment().substring(1);
-		try{
-		
-		  for(String s:tag.substring(1).split(", ")){
-			  i.add(Integer.parseInt(s));
-		  }
-		
-		  for(Integer ii: i){
-
-		  String aChar = new Character((char)(int)ii).toString();
-		  if(!(aChar.equals("")))
-		  string = string + aChar;
-		  string =  string.trim();
-		  }
-		  tag = string;
+		catch (NumberFormatException e) {
+			Log.getLogger().log(Level.SEVERE, "Failed to convert \"" + this.comment + "\" to a comment.", e);
 		}
-		catch(Exception e){
-			System.out.println("Exception tags");
-		}
-	
 	}
 	
+	public void convertTags() {
+		try {
+			this.tag = convertDecimalNumbersToString(this.tag);
+		}
+		catch (NumberFormatException e) {
+			Log.getLogger().log(Level.SEVERE, "Failed to convert \"" + this.tag + "\" to a tag.", e);
+		}
+	}
+	
+	/**
+	 * Converts a string that is on decimal format into a
+	 * "normal" string. The string may look something like 
+	 * "67, 101, 33, 33"
+	 * @param decimalString
+	 * @return
+	 * @throws NumberFormatException
+	 */
+	public String convertDecimalNumbersToString(String decimalString) throws NumberFormatException {
+		StringBuilder builder = new StringBuilder("");
+		List<Integer> i = new ArrayList<Integer>();
+		
+		String string = decimalString.substring(1);
+		for (String s: string.split(", ")) {
+			i.add(Integer.parseInt(s));
+		}
+
+		for (Integer ii: i) {
+			String aChar = new Character((char)(int)ii).toString();
+			aChar = aChar.trim();
+			builder.append(aChar);
+		}
+		
+		return builder.toString();
+	}
+
 
 	@Override
 	public void setLocation(String location) {
 		this.location=location;
 		locationObject = new LocationObject(location);
-		
+
 	}
 
 	@Override
 	public void setCoverPath(String coverPath) {
 		this.coverPath = coverPath;
-		
+
 	}
 
 	@Override
@@ -215,5 +215,5 @@ public class ImageObject implements FileObject {
 		return coverPath;
 	}
 
-	
+
 }
