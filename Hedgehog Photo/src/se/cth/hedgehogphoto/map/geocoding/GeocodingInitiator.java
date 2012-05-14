7737ed444279b610d7d610072fa3eb9396606aeb
FlorianMinges <florianminges@hotmail.com>
2012-05-14 19:03:19 +0200

    Added a geocoding-initiator
    which handles initialization of the geocoding-subsystem.
    (Currently using a JFrame for testing causes, this might change).

---
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/map/geocoding/GeocodingInitiator.java
@@ -0,0 +1,37 @@
+package se.cth.hedgehogphoto.map.geocoding;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import se.cth.hedgehogphoto.map.controller.MapController;
+import se.cth.hedgehogphoto.map.geocoding.controller.GeocodingController;
+import se.cth.hedgehogphoto.map.geocoding.view.GeoSearchPanel;
+import se.cth.hedgehogphoto.map.model.MapModel;
+import se.cth.hedgehogphoto.map.view.MapView;
+import se.cth.hedgehogphoto.plugin.InitializePlugin;
+import se.cth.hedgehogphoto.plugin.Panel;
+import se.cth.hedgehogphoto.plugin.Plugin;
+import se.cth.hedgehogphoto.view.PluginArea;
+
+public class GeocodingInitiator {
+	
+	public static void main(String [] args) {
+		new GeocodingInitiator();
+	}
+	
+	public GeocodingInitiator() {
+		GeoSearchPanel view = new GeoSearchPanel();
+		GeocodingController controller = new GeocodingController(view);
+		
+		JFrame frame = new JFrame();
+		frame.setPreferredSize(new Dimension(400,600));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(view);
+		frame.pack();
+		frame.getContentPane().setVisible(true);
+		frame.setVisible(true);
+		
+	}
+}
