d57c89862d811021e5900de2b36803b775443a73
FlorianMinges <florianminges@hotmail.com>
2012-05-16 14:57:05 +0200

    Changed class-name to something more proper

--- Hedgehog Photo/test/se/cth/hedgehogphoto/map/geocoding/model/FakeXMLParserMock.java
+++ Hedgehog Photo/test/se/cth/hedgehogphoto/map/geocoding/model/FakeXMLParserMock.java
@@ -3,17 +3,26 @@ package se.cth.hedgehogphoto.map.geocoding.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MaxOneRequestPerSecondTest implements Runnable {
-	private static MaxOneRequestPerSecondTest instance;
+/**
+ * This class represents the XMLParser. The reason
+ * I don't test it directly, is that I don't want to 
+ * make multiple requests to the nominatim-server,
+ * IF the tests would fail (since that would contradict 
+ * to the usage policy). This class is built in
+ * the same way, but doesn't process any XML documents.
+ * @author Florian Minges
+ */
+public class FakeXMLParserMock implements Runnable {
+	private static FakeXMLParserMock instance;
 	public static final long WAIT_MS = 100;
 	
-	public static synchronized MaxOneRequestPerSecondTest getInstance() {
+	public static synchronized FakeXMLParserMock getInstance() {
 		if (instance == null) 
-			instance = new MaxOneRequestPerSecondTest();
+			instance = new FakeXMLParserMock();
 		return instance;
 	}
 	
-	private MaxOneRequestPerSecondTest() {
+	private FakeXMLParserMock() {
 		
 	}
 
