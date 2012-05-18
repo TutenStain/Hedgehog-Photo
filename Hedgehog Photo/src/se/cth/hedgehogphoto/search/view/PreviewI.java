3fe873631e05951f80725d2d2a1e63a71c21ecde
FlorianMinges <florianminges@hotmail.com>
2012-05-18 18:29:53 +0200

    Just updated so I could compile
    Though I am not sure if we will really use this interface anymore...

--- Hedgehog Photo/src/se/cth/hedgehogphoto/search/view/PreviewI.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/search/view/PreviewI.java
@@ -1,7 +1,13 @@
 package se.cth.hedgehogphoto.search.view;
 
+import java.util.Observable;
+
+import javax.swing.JTextField;
+
 public interface PreviewI {
 	
 	//TODO: Add interface for JPopupPreview
+	public abstract void setTextField(JTextField t);
 
+	public abstract void update(Observable o, Object arg);
 }
