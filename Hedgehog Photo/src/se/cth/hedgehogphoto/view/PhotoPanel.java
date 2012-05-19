ad3545cde29270d1067f8aecfdb74ae60e1d07fc
David Grankvist <david.grankvist@hotmail.com>
2012-05-19 19:36:57 +0200

    unused import

--- Hedgehog Photo/src/se/cth/hedgehogphoto/view/PhotoPanel.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/view/PhotoPanel.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
 import java.util.List;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
