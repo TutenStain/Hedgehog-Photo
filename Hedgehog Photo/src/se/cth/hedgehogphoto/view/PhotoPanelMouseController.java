540e9f8005409ca82bbd4273efd6743a6ae92c69
David Grankvist <david.grankvist@hotmail.com>
2012-05-19 19:42:35 +0200

    cleanup

--- Hedgehog Photo/src/se/cth/hedgehogphoto/view/PhotoPanelMouseController.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/view/PhotoPanelMouseController.java
@@ -6,7 +6,6 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 
 public class PhotoPanelMouseController {
 	private PhotoPanel photoPanel;
@@ -16,14 +15,11 @@ public class PhotoPanelMouseController {
 		photoPanel.setMouseListener(new MouseAdapter(){
 			@Override
 			public void mouseClicked(MouseEvent e){
-				if(e.getSource() instanceof JLabel){
-					JLabel label = (JLabel) e.getSource();
 					Image image = photoPanel.getIcon().getImage();
 					BufferedImage bi = ImageUtils.resize(image, image.getWidth(null), image.getHeight(null));
 					ImageIcon icon2 = new ImageIcon(bi);
 					photoPanel.setIcon(icon2);
 				}
-			}
 		});
 	}
 }
