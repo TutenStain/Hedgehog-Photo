2e42487620ee0bc952244047d71fdd7003a3411f
David Grankvist <david.grankvist@hotmail.com>
2012-05-19 15:14:19 +0200

    Images now scale to their original size by clicking on them. This is just temporary

--- Hedgehog Photo/src/se/cth/hedgehogphoto/view/PhotoPanelMouseController.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/view/PhotoPanelMouseController.java
@@ -1,8 +1,11 @@
 package se.cth.hedgehogphoto.view;
 
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class PhotoPanelMouseController {
@@ -15,6 +18,10 @@ public class PhotoPanelMouseController {
 			public void mouseClicked(MouseEvent e){
 				if(e.getSource() instanceof JLabel){
 					JLabel label = (JLabel) e.getSource();
+					Image image = photoPanel.getIcon().getImage();
+					BufferedImage bi = ImageUtils.resize(image, image.getWidth(null), image.getHeight(null));
+					ImageIcon icon2 = new ImageIcon(bi);
+					photoPanel.setIcon(icon2);
 				}
 			}
 		});
