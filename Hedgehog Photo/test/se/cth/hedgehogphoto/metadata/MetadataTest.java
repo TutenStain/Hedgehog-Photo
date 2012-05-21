4dafea49eeb66cac56a02453f35ebeadb27cd14c
FlorianMinges <florianminges@hotmail.com>
2012-05-21 11:26:34 +0200

    Default implementation of tests for the metadata-class

---
+++ Hedgehog Photo/test/se/cth/hedgehogphoto/metadata/MetadataTest.java
@@ -0,0 +1,29 @@
+package se.cth.hedgehogphoto.metadata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MetadataTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetImageObjectFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExtractMetadata() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetImageObjectIImageMetadata() {
+		fail("Not yet implemented");
+	}
+
+}
