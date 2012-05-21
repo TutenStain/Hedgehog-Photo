01831dae05255b9de9348e40dccdc1781e0d61e3
FlorianMinges <florianminges@hotmail.com>
2012-05-21 11:24:45 +0200

    Default implementation of tests for the objects-package

---
+++ Hedgehog Photo/test/se/cth/hedgehogphoto/objects/ImageObjectTest.java
@@ -0,0 +1,149 @@
+package se.cth.hedgehogphoto.objects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImageObjectTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testImageObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProperty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAlbumName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAlbumName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTags() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTags() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArtist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFilePath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFilePath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFileName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFileName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLocationObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLocationObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvertComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvertTags() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvertDecimalNumbersToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCoverPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoverPath() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Hedgehog Photo/test/se/cth/hedgehogphoto/objects/LocationObjectOtherTest.java
@@ -0,0 +1,74 @@
+package se.cth.hedgehogphoto.objects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LocationObjectOtherTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testLocationObjectOtherString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLocationObjectOtherDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLocationString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLocationDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLatitudeDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLatitudeString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLongitudeDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLongitudeString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLatitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLongitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLocation() {
+		fail("Not yet implemented");
+	}
+
+}
