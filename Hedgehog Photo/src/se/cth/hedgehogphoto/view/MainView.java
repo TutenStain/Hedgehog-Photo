b627e9337d0d6962bacc63fce01a4bbf2a7108ff
David Grankvist <david.grankvist@hotmail.com>
2012-05-22 10:11:06 +0200

    mouse listener bugfix

--- Hedgehog Photo/src/se/cth/hedgehogphoto/view/MainView.java
+++ Hedgehog Photo/src/se/cth/hedgehogphoto/view/MainView.java
@@ -244,7 +244,7 @@ public class MainView implements Observer {
 	public void addPhotoPanelMouseListener(MouseAdapter listener) {
 		this.mouseListener = listener;
 		for (PhotoPanel panel : this.photoPanels) {
-			panel.addMouseListener(listener);
+			panel.setMouseListener(listener);
 		}
 	}
 
